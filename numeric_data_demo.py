933dde7f1980be1731a92ca33b46b2466c1bcaa4
Vladimir Bredikhin <vladimir.bredikhin@broscorp.net>
2019-11-05 16:04:10 +0200

    Add numeric data operations and functions

---
+++ numeric_data_demo.py
@@ -0,0 +1,18 @@
+print(f'''
+Arithmetic Operators:
+    # Addiction:          3 + 2 = {3 + 2}
+    # Subtraction:        3 - 2 = {3 - 2}
+    # Multiplication:     3 * 2 = {3 * 2}
+    # Division:           3 / 2 = {3 / 2}            
+    # Floor division:     3 // 2 = {3 // 2}
+    # Exponent:           3 ** 2 = {3 ** 2}
+    # Modulus:            3 % 2 = {3 % 2}
+    
+Builtin functions for numeric data:
+    # abs(-3) = {abs(-3)}
+    # round(3.75, 1) = {round(3.75, 1)}
+
+Casting:
+    # '100' + '200' = {'100' + '200'}
+    # int('100') + int('200') = {int('100') + int('200')}
+''')
