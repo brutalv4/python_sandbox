33f48629f8fc3a6ec1f633a87ca4d8ca5d75de31
Vladimir Bredikhin <vladimir.bredikhin@broscorp.net>
2019-11-11 18:10:01 +0200

    Add variable scope LEGB

---
+++ variable_scope_LEGB/demo.py
@@ -0,0 +1,21 @@
+"""
+LEGB
+Local, Enclosing, Global, Built-in
+"""
+
+x = 'global x'
+
+
+def outer():
+    x = 'outer x'
+
+    def inner():
+        x = 'inner x'
+        print(x)
+
+    inner()
+    print(x)
+
+
+outer()
+print(x)
